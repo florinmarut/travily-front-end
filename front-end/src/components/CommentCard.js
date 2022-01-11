8f1e361dd7789647c25a31aa16b9286b7b23d393
Florin Măruț <florinmarut@gmail.com>
2022-01-11 12:19:34 +0200

    Feed button now redirects to feed, surround comments in cards

--- front-end/src/components/CommentCard.js
+++ front-end/src/components/CommentCard.js
@@ -1,15 +1,21 @@
 import { UserMiniCard } from "./UserMiniCard";
-
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 export const CommentCard = ({postId, userId, content, votes, date, updateDate}) => {
     return (
-        <div>
+<Card sx={{ maxWidth: 500 }} style={{ margin: '25px auto 75px auto'}}>
+      <CardContent>
+      <div>
             <UserMiniCard userId={userId}/>
             <p>Content: {content}</p>
             <p>Votes: {votes}</p>
             <p>Created at: {date}</p>
             <p>Updated at: {updateDate}</p>
         </div>
+      </CardContent>
+      
+    </Card>
     )
 }
 
--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -36,7 +36,7 @@ function Navbar() {
           </div>
          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
            <li className='nav-item'>
-             <Link to='/' 
+             <Link to='/feed' 
               className='nav-links'  
               onClick={closeMobileMenu}>Feed</Link></li>
               <li className='nav-item'>
